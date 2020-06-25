b1e84be6af45329c7cf9f59603ad945d8e158982
Charles Reid <charlesreid1@gmail.com>
2020-06-25 00:15:46 -0700

    use working XFF header

--- Fail2banlog.php
+++ Fail2banlog.php
@@ -18,19 +18,10 @@ function logBadLogin($response, $user, $username) {
 
     if ( $response->status == "PASS" ) return true; // Do not log success or password send request, continue to next hook
     $time = date ("Y-m-d H:i:s T");
-    $remote_addr_ip = $_SERVER['REMOTE_ADDR'];
-    $get_ip = $wgRequest->getIP();
-    $xff = $wgRequest->getHeader( 'X-Forwarded-For' );
-    $xff2 = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    $client_ip = $_SERVER['HTTP_CLIENT_IP'];
+    $remote_ip = $wgRequest->getHeader('X-Forwarded-For');
 
-    // append a line to the log
-    error_log("$time MediaWiki authentication error from IP:");
-    error_log("$time - _SERVER remote address: $remote_addr_ip");
-    error_log("$time - wgrequest getIP(): $get_ip");
-    error_log("$time - wgrequest getheader xff: $xff");
-    error_log("$time - _SERVER xff: $xff2");
-    error_log("$time - _SERVER client ip: $client_ip");
+    // append a line to the logs
+    error_log("$time MediaWiki authentication error: failed login attempt from IP $remote_ip");
 
     return true; // continue to next hook
 }
