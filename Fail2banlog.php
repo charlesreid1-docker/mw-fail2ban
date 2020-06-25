af6f8f4d91e9da2a7c9fed4c617a13fb0071ddb1
Charles Reid <charlesreid1@gmail.com>
2020-06-25 00:08:48 -0700

    enumerate all possible IPs

--- Fail2banlog.php
+++ Fail2banlog.php
@@ -21,10 +21,17 @@ function logBadLogin($response, $user, $username) {
     $remote_addr_ip = $_SERVER['REMOTE_ADDR'];
     $get_ip = $wgRequest->getIP();
     $xff = $wgRequest->getHeader( 'X-Forwarded-For' );
-    // wfGetIP() may yield different results for proxies
+    $xff2 = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $client_ip = $_SERVER['HTTP_CLIENT_IP'];
 
     // append a line to the log
-    error_log("$time MediaWiki authentication error from $remote_addr_ip (remote addr) or $get_ip (wgRequest->getIP()) or $xff (xff header)\n",0);
+    error_log("$time MediaWiki authentication error from IP:");
+    error_log("$time - _SERVER remote address: $remote_addr_ip");
+    error_log("$time - wgrequest getIP(): $get_ip");
+    error_log("$time - wgrequest getheader xff: $xff");
+    error_log("$time - _SERVER xff: $xff2");
+    error_log("$time - _SERVER client ip: $client_ip");
+
     return true; // continue to next hook
 }
 ?>
